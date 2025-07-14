b8b89074c0b2376b767a5a577e838b4a132a17ec
kleva-j <kasmickleva@gmail.com>
2025-07-14 10:27:20 +0100

    feat: remove eslint and prettier config and setup biome with ultracite

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -1,22 +1,24 @@
-import { contextBridge } from 'electron'
-import { electronAPI } from '@electron-toolkit/preload'
+import { electronAPI } from '@electron-toolkit/preload';
+import { contextBridge } from 'electron';
+
+const { error: logError } = console;
 
 // Custom APIs for renderer
-const api = {}
+const api = {};
 
 // Use `contextBridge` APIs to expose Electron APIs to
 // renderer only if context isolation is enabled, otherwise
 // just add to the DOM global.
 if (process.contextIsolated) {
   try {
-    contextBridge.exposeInMainWorld('electron', electronAPI)
-    contextBridge.exposeInMainWorld('api', api)
+    contextBridge.exposeInMainWorld('electron', electronAPI);
+    contextBridge.exposeInMainWorld('api', api);
   } catch (error) {
-    console.error(error)
+    logError(error);
   }
 } else {
   // @ts-expect-error (define in dts)
-  window.electron = electronAPI
+  window.electron = electronAPI;
   // @ts-expect-error (define in dts)
-  window.api = api
+  window.api = api;
 }
